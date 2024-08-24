5f8c1e0a937473914b403dce1d6319ef933830a0
Phasin Sae-ngow <bird.biob3@gmail.com>
2024-08-24 15:46:56 +0700

    Implement tutorial part 2

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,34 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        """
+        :return: The text of the question
+        """
+        return self.question_text
+
+    def was_published_recently(self):
+        """
+        Check whether the question was published within the last 24 hours.
+        
+        :return: True if pub_date is within the last 24 hours,
+                 False otherwise
+        """
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        """
+        :return: The text of the choice
+        """
+        return self.choice_text
