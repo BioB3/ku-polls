76bf870d6709cdfae2dcca47dcc3e402831e51e3
Phasin Sae-ngow <bird.biob3@gmail.com>
2024-08-25 13:51:25 +0700

    fix typo in tests

--- polls/tests.py
+++ polls/tests.py
@@ -78,7 +78,7 @@ class QuestionDetailViewTests(TestCase):
         returns a 404 not found.
         """
         future_question = create_question(question_text='Future question.', days=5)
-        url = reverse('polls:index', args=(future_question.id,))
+        url = reverse('polls:detail', args=(future_question.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
@@ -88,7 +88,7 @@ class QuestionDetailViewTests(TestCase):
         displays the question's text.
         """
         past_question = create_question(question_text='Past question.', days=-5)
-        url = reverse('polls:index', args=(past_question.id,))
+        url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
 
