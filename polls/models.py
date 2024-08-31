f63c452325e4336ca16e17d6b3f23cf405f49dc0
Phasin Sae-ngow <bird.biob3@gmail.com>
2024-08-31 15:56:54 +0700

    added end_date and default value to pub_date

--- polls/models.py
+++ polls/models.py
@@ -12,8 +12,9 @@ class Question(models.Model):
     Stores the text and publication date of questions as a table in the database.
     """
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
+    pub_date = models.DateTimeField('date published', default=timezone.now)
+    end_date = models.DateTimeField('ending date for voting',
+                                    default=None, null=True, blank=True)
     def __str__(self):
         """
         :return: The text of the question
@@ -29,6 +30,29 @@ class Question(models.Model):
         """
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def is_published(self):
+        """
+        Check whether the question is published.
+        
+        :return: True if current date-time is on or after question's publication date,
+                 False otherwise
+        """
+        now = timezone.now()
+        return now >= self.pub_date
+
+    def can_vote(self):
+        """
+        Check whether voting is available for the question.
+        
+        :return: True if voting is allowed for this question,
+                 False otherwise
+        """
+        now = timezone.now()
+        if self.end_date is not None:
+            return self.end_date >= now >= self.pub_date
+        else:
+            return now >= self.pub_date
 
 
 class Choice(models.Model):
