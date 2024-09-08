df503e1e41264943426d5982f470471e3c79d2d4
Phasin Sae-ngow <bird.biob3@gmail.com>
2024-09-08 11:13:38 +0700

    implemented logging

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,3 +130,38 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "class": "logging.FileHandler",
+            "filename": "polls.log",
+            "level": "DEBUG",
+            "formatter": "details",
+        },
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": "INFO",
+            "formatter": "simple",
+        },
+    },
+    "loggers": {
+        "polls": {
+            "handlers": ["file", "console"],
+            "level": "DEBUG",
+            "propagate": True,
+        },
+    },
+    "formatters": {
+        "details": {
+            "format": "{levelname} {asctime} {module} {message}",
+            "style": "{",
+        },
+        "simple": {
+            "format": "{levelname} {message}",
+            "style": "{",
+        },
+    },
+}
--- polls/views.py
+++ polls/views.py
@@ -5,8 +5,13 @@ from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 from django.contrib import messages
+from django.contrib.auth.signals import user_logged_in, user_logged_out, user_login_failed
 from django.contrib.auth.decorators import login_required
+from django.dispatch import receiver
 from .models import Question, Choice, Vote
+import logging
+
+logger = logging.getLogger("polls")
 
 
 class IndexView(generic.ListView):
@@ -19,12 +24,12 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions.
+        """Return published questions ordered by publication date.
 
         (not including those set to be published in the future).
         """
         return Question.objects.filter(
-            pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+            pub_date__lte=timezone.now()).order_by('-pub_date')
 
 
 class DetailView(generic.DetailView):
@@ -54,7 +59,8 @@ class DetailView(generic.DetailView):
         """
         try:
             question = get_object_or_404(Question, pk=kwargs["pk"])
-        except Http404:
+        except Http404 as ex:
+            logger.exception(f"Non-existent question {kwargs['pk']} %s", ex)
             messages.error(request, f"Poll ID {kwargs['pk']} does not exist.")
             return HttpResponseRedirect(reverse("polls:index"))
         if not question.can_vote():
@@ -66,11 +72,17 @@ class DetailView(generic.DetailView):
     def get_context_data(self, **kwargs):
         """Create context dictionary used to render the template."""
         context = super().get_context_data(**kwargs)
+        user = self.request.user
         try: 
             selected_choice = kwargs["object"].choice_set.filter(
-                vote__user=self.request.user).first()
-            vote = Vote.objects.get(user=self.request.user, choice=selected_choice)
-        except (TypeError, KeyError, Vote.DoesNotExist):
+                vote__user=user).first()
+            vote = Vote.objects.get(user=user, choice=selected_choice)
+        except (KeyError, Vote.DoesNotExist):
+            logger.exception(f"Vote for question #{kwargs['object'].id}"
+                             f" from {user.username} does not exist")
+            vote = None
+        except TypeError:
+            logger.exception(f"Cannot retrieve vote for unauthenticated visitor")
             vote = None
         context['vote'] = vote
         return context
@@ -98,7 +110,8 @@ class ResultsView(generic.DetailView):
         """
         try:
             question = get_object_or_404(Question, pk=kwargs["pk"])
-        except Http404:
+        except Http404 as ex:
+            logger.exception(f"Non-existent question {kwargs['pk']} %s", ex)
             messages.error(request, f"Poll ID {kwargs['pk']} does not exist.")
             return HttpResponseRedirect(reverse("polls:index"))
         if not question.is_published():
@@ -117,12 +130,17 @@ def vote(request, question_id):
              the detail page with error message if no choice was selected
     """
     question = get_object_or_404(Question, pk=question_id)
+    user = request.user
     try:
+        logger.info(f"{user.username} voted for choice "
+                    f"{request.POST['choice']} in question {question_id}")
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
+        logger.exception(f"{user.username} did not select a choice")
         messages.error(request, "You didn't select a choice.")
         return HttpResponseRedirect(reverse('polls:detail',
                                             args=(question_id,)))
+
     try:
         vote = request.user.vote_set.get(choice__question=question)
         vote.choice = selected_choice
@@ -133,4 +151,32 @@ def vote(request, question_id):
         vote = Vote.objects.create(user=request.user, choice=selected_choice)
         messages.success(request,
                          f"You voted for '{selected_choice.choice_text}'.")
+
     return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
+
+def get_client_ip(request):
+    """Get the visitor's IP address using request headers."""
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+@receiver(user_logged_in)
+def user_login(sender, request, user, **kwargs):
+    """Log successful login."""
+    ip = get_client_ip(request)
+    logger.info(f"user {user.username} logged in via ip: {ip}")
+
+@receiver(user_logged_out)
+def user_logout(sender, request, user, **kwargs):
+    """Log successful logout."""
+    ip = get_client_ip(request)
+    logger.info(f"user {user.username} logged out via ip: {ip}")
+
+@receiver(user_login_failed)
+def user_login_failed(sender, request, credentials, **kwargs):
+    """Log unsuccessful login"""
+    ip = get_client_ip(request)
+    logger.warning(f"login failed for {credentials['username']} from ip: {ip}")
